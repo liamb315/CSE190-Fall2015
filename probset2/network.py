f904b29fcdf2e884a9b727587e8726204dd6ee9b
William Fedus <liam.fedus@gmail.com>
2015-10-27 11:00:17 -0700

    Backprop almost working

--- probset2/network.py
+++ probset2/network.py
@@ -10,26 +10,51 @@ class network:
 		self.topology   = topology
 		self.num_layers = len(topology)
 		self.weights    = [np.random.randn(y,x) for x,y in zip(topology[:-1], topology[1:])] 
+		self.a          = [np.zeros(l) for l in topology]
+		self.z          = [np.zeros(l) for l in topology]
+		
 
-	def forward(self, a):
+	def forward(self, x):
 		'''Forward propagation'''
-		for w in zip(self.weights):
-			a = sigmoid(np.dot(w, a))
-		return a
+		self.a[0] = x
+		self.z[0] = x
+		for l in range(0, len(self.weights)):
+			self.a[l+1] = np.dot(self.weights[l], x)
+			x = sigmoid(np.dot(self.weights[l], x))
+			self.z[l+1] = x
+		return x
+	
 
-	def backward(self):
+	def backward(self, x, t):
 		'''Backward propagation'''
-		pass
+		grad_w      = [np.zeros(w.shape) for w in self.weights]
+		delta       = [np.zeros(l) for l in self.topology]
+		N           = self.num_layers
 
-	def loss(self):
+		# Compute delta for output layer
+		delta[N-1]  = self.z[N-1] - t
+		grad_w[N-2] = np.dot(delta[N-1], self.z[N-2].T)
+		
+		# Compute delta for hidden layers
+		for l in range(N-2, -1, -1):
+			delta[l]    = np.dot(self.weights[l].T, delta[l+1]) * sigmoid_derivative(self.a[l])
+			grad_w[l-1] = np.dot(delta[l], self.z[l-1].T)
+		return grad_w
+
+
+	def error(self):
 		'''Loss for the network '''
 		pass
 
-	def get_parameters(self):
-		return self.weights
+
+	def error_derivative(self):
+		pass
 
 
 
 def sigmoid(a):
 	return 1.0/(1.0 + np.exp(-a))
+
+def sigmoid_derivative(a):
+	return sigmoid(a) * (1.0 - sigmoid(a))
 
--- probset2/test.py
+++ probset2/test.py
@@ -1,9 +1,22 @@
 import numpy as np
 import network as nn
 
-
+# Data example
 x = np.array([[1,1]]).T
-net = nn.network([2,3,1])
-print net.get_parameters()
+t = 1.0
 
+# Establish a network
+net = nn.network([2,3,1])
+
+#print net.a
+#print net.z
 print net.forward(x)
+
+grad_w = net.backward(x, t)
+
+for i in range(0, len(net.weights)):
+	net.weights[i] = net.weights[i] - 0.01 * grad_w[i]
+
+print
+print net.forward(x)
+
