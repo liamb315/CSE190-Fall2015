4d2252751e2943915b5cb5329cc3fda7fa3adec7
William Fedus <liam.fedus@gmail.com>
2015-10-27 14:32:49 -0700

    Added biases

--- probset2/network.py
+++ probset2/network.py
@@ -10,6 +10,7 @@ class network:
 		self.topology   = topology
 		self.num_layers = len(topology)
 		self.weights    = [np.random.randn(y,x) for x,y in zip(topology[:-1], topology[1:])] 
+		self.biases     = [np.random.randn(y, 1) for y in topology[1:]]
 		self.a          = [np.zeros(l) for l in topology]
 		self.z          = [np.zeros(l) for l in topology]
 		
@@ -19,12 +20,8 @@ class network:
 		self.a[0] = x
 		self.z[0] = x
 		for l in range(0, len(self.weights)):
-			self.a[l+1] = np.dot(self.weights[l], x)
-			#print 'x before'
-			#print x
-			x = sigmoid(np.dot(self.weights[l], x))
-			#print 'x after'
-			#print x
+			self.a[l+1] = np.dot(self.weights[l], x) + self.biases[l]
+			x = sigmoid(np.dot(self.weights[l], x) + self.biases[l])
 			self.z[l+1] = x
 		print 'result:'
 		return x
@@ -33,18 +30,21 @@ class network:
 	def backward(self, x, t):
 		'''Backward propagation'''
 		grad_w      = [np.zeros(w.shape) for w in self.weights]
+		grad_b      = [np.zeros(b.shape) for b in self.biases ]
 		delta       = [np.zeros(l) for l in self.topology]
 		N           = self.num_layers
 
 		# Compute delta for output layer
 		delta[N-1]  = self.z[N-1] - t
 		grad_w[N-2] = np.dot(delta[N-1], self.z[N-2].T)
+		grad_b[N-2] = delta[N-1]
 		
 		# Compute delta for hidden layers
 		for l in range(N-2, 0, -1):
 			delta[l]    = np.dot(self.weights[l].T, delta[l+1]) * sigmoid_derivative(self.a[l])
 			grad_w[l-1] = np.dot(delta[l], self.z[l-1].T)
-		return grad_w
+			grad_b[l-1] = delta[l]
+		return grad_w, grad_b
 
 
 	def error(self):
--- probset2/test.py
+++ probset2/test.py
@@ -6,22 +6,22 @@ x = np.array([[1,1]]).T
 t = 1.0
 
 # Establish a network
-net = nn.network([2,3,1])
+net = nn.network([2,3,5,1])
 
 
 print net.forward(x)
-
-grad_w = net.backward(x, t)
+grad_w, grad_b = net.backward(x, t)
 
 
 for i in range(0, len(net.weights)):
 	net.weights[i] = net.weights[i] - 1.0 * grad_w[i]
-
-
+	net.biases[i]  = net.biases[i] - 1.0 * grad_b[i]
 
 print net.forward(x)
-grad_w = net.backward(x, t)
+grad_w, grad_b = net.backward(x, t)
+
 for i in range(0, len(net.weights)):
 	net.weights[i] = net.weights[i] - 1.0 * grad_w[i]
+	net.biases[i]  = net.biases[i] - 1.0 * grad_b[i]
 
 print net.forward(x)
